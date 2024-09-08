1912aa254d85b4c2d0e8620c4e18bb539ce98eca
SiddhiKamdar <siddhikamdar00@gmail.com>
2024-09-08 13:52:20 +0530

    Edit Functionality in Product Table Add Edit in User Table

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,185 +1,251 @@
-﻿using CoffeeShopManagment.Models;
-using Microsoft.AspNetCore.Mvc;
-using OfficeOpenXml;
-using System.Data;
-using System.Data.SqlClient;
+﻿    using CoffeeShopManagment.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using OfficeOpenXml;
+    using System.Data;
+    using System.Data.SqlClient;
 
-namespace CoffeeShopManagment.Controllers
-{
-    public class ProductController : Controller
+    namespace CoffeeShopManagment.Controllers
     {
-        private IConfiguration configuration;
-
-        public ProductController(IConfiguration _configuration)
+        public class ProductController : Controller
         {
-            configuration = _configuration;
-        }
-        public ActionResult ExportToExcel()
-        {
- 
-            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            private IConfiguration configuration;
 
-            DataTable dataTable = GetProductsData(); 
-
-            using (ExcelPackage package = new ExcelPackage())
+            public ProductController(IConfiguration _configuration)
             {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Products");
+                configuration = _configuration;
+            }
+            public ActionResult ExportToExcel()
+            {
+ 
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-                for (int col = 1; col <= dataTable.Columns.Count; col++)
-                {
-                    worksheet.Cells[1, col].Value = dataTable.Columns[col - 1].ColumnName;
-                }
+                DataTable dataTable = GetProductsData(); 
 
-                for (int row = 1; row <= dataTable.Rows.Count; row++)
+                using (ExcelPackage package = new ExcelPackage())
                 {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Products");
+
                     for (int col = 1; col <= dataTable.Columns.Count; col++)
                     {
-                        worksheet.Cells[row + 1, col].Value = dataTable.Rows[row - 1][col - 1];
+                        worksheet.Cells[1, col].Value = dataTable.Columns[col - 1].ColumnName;
                     }
-                }
 
-                var stream = new MemoryStream(package.GetAsByteArray());
-                string fileName = "Products.xlsx";
-                string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-
-                return File(stream, contentType, fileName);
-            }
-        }
-
-
-        private DataTable GetProductsData()
-        {
-            string connectionString = this.configuration.GetConnectionString("ConnectionString");
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-          
-                using (SqlCommand sqlCommand = connection.CreateCommand())
-                {
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    sqlCommand.CommandText = "PR_Product_SelectAll";
-
-                    
-                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                    for (int row = 1; row <= dataTable.Rows.Count; row++)
                     {
-                        DataTable dataTable = new DataTable();
-                        dataTable.Load(reader);
-                        return dataTable;
+                        for (int col = 1; col <= dataTable.Columns.Count; col++)
+                        {
+                            worksheet.Cells[row + 1, col].Value = dataTable.Rows[row - 1][col - 1];
+                        }
                     }
+
+                    var stream = new MemoryStream(package.GetAsByteArray());
+                    string fileName = "Products.xlsx";
+                    string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+                    return File(stream, contentType, fileName);
                 }
             }
-        }
 
 
-        public IActionResult Index()
-        {
-            string connectionString = this.configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand sqlCommand = connection.CreateCommand();
-            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-            sqlCommand.CommandText = "PR_Product_SelectAll";
-            SqlDataReader reader = sqlCommand.ExecuteReader();
-            DataTable dataTable = new DataTable();
-            dataTable.Load(reader);
-            //return View(productList);
-            return View(dataTable);
-        }
-        public IActionResult Form()
-        {
-            string connectionString = this.configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection2 = new SqlConnection(connectionString);
-            connection2.Open();
-            SqlCommand command2 = connection2.CreateCommand();
-            command2.CommandType = System.Data.CommandType.StoredProcedure;
-            command2.CommandText = "PR_User_DropDown";
-            SqlDataReader reader2 = command2.ExecuteReader();
-            DataTable dataTable2 = new DataTable();
-            dataTable2.Load(reader2);
-            List<UserDropDownModel> userList = new List<UserDropDownModel>();
-            foreach (DataRow data in dataTable2.Rows)
-            {
-                UserDropDownModel userDropDownModel = new UserDropDownModel();
-                userDropDownModel.UserID = Convert.ToInt32(data["UserID"]);
-                userDropDownModel.UserName = data["UserName"].ToString();
-                userList.Add(userDropDownModel);
-            }
-            ViewBag.UserList = userList;
-            return View();
-        }
-        public IActionResult ProductSave(ProductModel productModel)
-        {
-            if (ModelState.IsValid)
+            private DataTable GetProductsData()
             {
                 string connectionString = this.configuration.GetConnectionString("ConnectionString");
+
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
 
-                    using (SqlCommand checkUserCommand = connection.CreateCommand())
+          
+                    using (SqlCommand sqlCommand = connection.CreateCommand())
                     {
-                        checkUserCommand.CommandType = CommandType.Text;
-                        checkUserCommand.CommandText = "SELECT COUNT(*) FROM [dbo].[User] WHERE UserID = @UserId";
-                        checkUserCommand.Parameters.Add("@UserId", SqlDbType.Int).Value = productModel.UserId;
+                        sqlCommand.CommandType = CommandType.StoredProcedure;
+                        sqlCommand.CommandText = "PR_Product_SelectAll";
 
-                        int userCount = (int)checkUserCommand.ExecuteScalar();
-                        if (userCount == 0)
+                    
+                        using (SqlDataReader reader = sqlCommand.ExecuteReader())
                         {
-                            ModelState.AddModelError("", "Invalid UserId");
-                            return View("Index", GetProductsData());
+                            DataTable dataTable = new DataTable();
+                            dataTable.Load(reader);
+                            return dataTable;
                         }
-                    }
-
-                    using (SqlCommand command = connection.CreateCommand())
-                    {
-                        command.CommandType = CommandType.StoredProcedure;
-                        if (productModel.ProductId == null)
-                        {
-                            command.CommandText = "PR_Product_Insert";
-                        }
-                        else
-                        {
-                            command.CommandText = "PR_Product_Update";
-                            command.Parameters.Add("@ProductId", SqlDbType.Int).Value = productModel.ProductId;
-                        }
-                        command.Parameters.Add("@ProductName", SqlDbType.VarChar).Value = productModel.ProductName;
-                        command.Parameters.Add("@ProductCode", SqlDbType.VarChar).Value = productModel.ProductCode;
-                        command.Parameters.Add("@ProductPrice", SqlDbType.Decimal).Value = productModel.ProductPrice;
-                        command.Parameters.Add("@Description", SqlDbType.VarChar).Value = productModel.Description;
-                        command.Parameters.Add("@UserId", SqlDbType.Int).Value = productModel.UserId;
-
-                        command.ExecuteNonQuery();
                     }
                 }
-                return RedirectToAction("Index");
             }
 
-            DataTable dataTable = GetProductsData();
-            return View("Index", dataTable);
-        }
 
-
-        public IActionResult ProductDelete(int ProductID)
-        {
-            try
+            public IActionResult Index()
             {
                 string connectionString = this.configuration.GetConnectionString("ConnectionString");
                 SqlConnection connection = new SqlConnection(connectionString);
                 connection.Open();
-                SqlCommand command = connection.CreateCommand();
-                command.CommandType = CommandType.StoredProcedure;
-                command.CommandText = "PR_Product_Delete";
-                command.Parameters.Add("@ProductID", SqlDbType.Int).Value = ProductID;
-                command.ExecuteNonQuery();
-                return RedirectToAction("Index");
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCommand.CommandText = "PR_Product_SelectAll";
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                return View(dataTable);
             }
-            catch (Exception ex) {
-                TempData["Error Message"] = ex.Message;
-                return RedirectToAction("Index");
+            public IActionResult Form()
+            {
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                SqlConnection connection2 = new SqlConnection(connectionString);
+                connection2.Open();
+                SqlCommand command2 = connection2.CreateCommand();
+                command2.CommandType = System.Data.CommandType.StoredProcedure;
+                command2.CommandText = "PR_User_DropDown";
+                SqlDataReader reader2 = command2.ExecuteReader();
+                DataTable dataTable2 = new DataTable();
+                dataTable2.Load(reader2);
+                List<UserDropDownModel> userList = new List<UserDropDownModel>();
+                foreach (DataRow data in dataTable2.Rows)
+                {
+                    UserDropDownModel userDropDownModel = new UserDropDownModel();
+                    userDropDownModel.UserID = Convert.ToInt32(data["UserID"]);
+                    userDropDownModel.UserName = data["UserName"].ToString();
+                    userList.Add(userDropDownModel);
+                }
+                ViewBag.UserList = userList;
+                return View();
+            }
+            public IActionResult ProductSave(ProductModel productModel)
+            {
+                if (ModelState.IsValid)
+                {
+                    string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                        using (SqlCommand checkUserCommand = connection.CreateCommand())
+                        {
+                            checkUserCommand.CommandType = CommandType.Text;
+                            checkUserCommand.CommandText = "SELECT COUNT(*) FROM [dbo].[User] WHERE UserID = @UserId";
+                            checkUserCommand.Parameters.Add("@UserId", SqlDbType.Int).Value = productModel.UserId;
+
+                            int userCount = (int)checkUserCommand.ExecuteScalar();
+                            if (userCount == 0)
+                            {
+                                ModelState.AddModelError("", "Invalid UserId");
+                                return View("Index", GetProductsData());
+                            }
+                        }
+
+                        using (SqlCommand command = connection.CreateCommand())
+                        {
+                            command.CommandType = CommandType.StoredProcedure;
+                            if (productModel.ProductId == null)
+                            {
+                                command.CommandText = "PR_Product_Insert";
+                            }
+                            else
+                            {
+                                command.CommandText = "PR_Product_Update";
+                                command.Parameters.Add("@ProductId", SqlDbType.Int).Value = productModel.ProductId;
+                            }
+                            command.Parameters.Add("@ProductName", SqlDbType.VarChar).Value = productModel.ProductName;
+                            command.Parameters.Add("@ProductCode", SqlDbType.VarChar).Value = productModel.ProductCode;
+                            command.Parameters.Add("@ProductPrice", SqlDbType.Decimal).Value = productModel.ProductPrice;
+                            command.Parameters.Add("@Description", SqlDbType.VarChar).Value = productModel.Description;
+                            command.Parameters.Add("@UserId", SqlDbType.Int).Value = productModel.UserId;
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    return RedirectToAction("Index");
+                }
+
+                DataTable dataTable = GetProductsData();
+                return View("Index", dataTable);
+            }
+            public IActionResult Edit(int ProductId)
+            {
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "PR_Product_SelectByPK"; 
+                        command.Parameters.Add("@ProductId", SqlDbType.Int).Value = ProductId;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                ProductModel productModel = new ProductModel
+                                {
+                                    ProductId = Convert.ToInt32(reader["ProductId"]),
+                                    ProductName = reader["ProductName"].ToString(),
+                                    ProductCode = reader["ProductCode"].ToString(),
+                                    ProductPrice = Convert.ToDecimal(reader["ProductPrice"]),
+                                    Description = reader["Description"].ToString(),
+                                    UserId = Convert.ToInt32(reader["UserId"])
+                                };
+
+                                LoadUserDropdown();
+
+                                return View("Form", productModel);
+                            }
+                        }
+                    }
+                }
+
+                return RedirectToAction("Index"); 
             }
 
+            private void LoadUserDropdown()
+            {
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "PR_User_DropDown";
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            DataTable dataTable = new DataTable();
+                            dataTable.Load(reader);
+                            List<UserDropDownModel> userList = new List<UserDropDownModel>();
+                            foreach (DataRow data in dataTable.Rows)
+                            {
+                                UserDropDownModel userDropDownModel = new UserDropDownModel();
+                                userDropDownModel.UserID = Convert.ToInt32(data["UserID"]);
+                                userDropDownModel.UserName = data["UserName"].ToString();
+                                userList.Add(userDropDownModel);
+                            }
+                            ViewBag.UserList = userList;
+                        }
+                    }
+                }
+            }
+
+
+            public IActionResult ProductDelete(int ProductID)
+            {
+                try
+                {
+                    string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "PR_Product_Delete";
+                    command.Parameters.Add("@ProductID", SqlDbType.Int).Value = ProductID;
+                    command.ExecuteNonQuery();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex) {
+                    TempData["Error Message"] = ex.Message;
+                    return RedirectToAction("Index");
+                }
+
+            }
         }
     }
-}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,68 +1,136 @@
 ﻿using CoffeeShopManagment.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace CoffeeShopManagment.Controllers
 {
     public class UserController : Controller
     {
-        private IConfiguration configuration;
+        private readonly IConfiguration configuration;
 
         public UserController(IConfiguration _configuration)
         {
             configuration = _configuration;
         }
+
         public IActionResult Index()
         {
             string connectionString = this.configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand sqlCommand = connection.CreateCommand();
-            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
-            sqlCommand.CommandText = "PR_User_SelectAll";
-            SqlDataReader reader = sqlCommand.ExecuteReader();
-            DataTable dataTable = new DataTable();
-            dataTable.Load(reader);
-            return View(dataTable);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = connection.CreateCommand();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = "PR_User_SelectAll";
+                using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    return View(dataTable);
+                }
+            }
         }
-        public IActionResult Form()
+
+        public IActionResult Form(int? userId)
         {
-            
-            return View();
+            if (userId.HasValue)
+            {
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = "PR_User_SelectByPK";
+                        command.Parameters.Add("@UserId", SqlDbType.Int).Value = userId.Value;
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                UserModel userModel = new UserModel
+                                {
+                                    UserId = Convert.ToInt32(reader["UserId"]),
+                                    UserName = reader["UserName"].ToString(),
+                                    Email = reader["Email"].ToString(),
+                                    Password = reader["Password"].ToString(),
+                                    MobileNo = reader["MobileNo"].ToString(),
+                                    Address = reader["Address"].ToString(),
+                                    IsActive = Convert.ToBoolean(reader["IsActive"])
+                                };
+
+                                return View(userModel);
+                            }
+                        }
+                    }
+                }
+            }
+            return View(new UserModel());
         }
-        public IActionResult UserDelete(int UserID)
+
+        [HttpPost]
+        public IActionResult UserSave(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = this.configuration.GetConnectionString("ConnectionString");
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        if (userModel.UserId == 0)
+                        {
+                            command.CommandText = "PR_User_Insert";
+                        }
+                        else
+                        {
+                            command.CommandText = "PR_User_Update";
+                            command.Parameters.Add("@UserId", SqlDbType.Int).Value = userModel.UserId;
+                        }
+
+                        command.Parameters.Add("@UserName", SqlDbType.VarChar).Value = userModel.UserName;
+                        command.Parameters.Add("@Email", SqlDbType.VarChar).Value = userModel.Email;
+                        command.Parameters.Add("@Password", SqlDbType.VarChar).Value = userModel.Password;
+                        command.Parameters.Add("@MobileNo", SqlDbType.VarChar).Value = userModel.MobileNo;
+                        command.Parameters.Add("@Address", SqlDbType.VarChar).Value = userModel.Address;
+                        command.Parameters.Add("@IsActive", SqlDbType.Bit).Value = userModel.IsActive;
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View("Form", userModel);
+        }
+
+        [HttpPost]
+        public IActionResult UserDelete(int userId)
         {
             try
             {
                 string connectionString = this.configuration.GetConnectionString("ConnectionString");
-                SqlConnection connection = new SqlConnection(connectionString);
-                connection.Open();
-                SqlCommand command = connection.CreateCommand();
-                command.CommandType = CommandType.StoredProcedure;
-                command.CommandText = "PR_User_Delete";
-                command.Parameters.Add("@ProductID", SqlDbType.Int).Value = UserID;
-                command.ExecuteNonQuery();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "PR_User_Delete";
+                    command.Parameters.Add("@UserId", SqlDbType.Int).Value = userId;
+                    command.ExecuteNonQuery();
+                }
                 return RedirectToAction("Index");
             }
             catch (Exception ex)
             {
                 TempData["Error Message"] = ex.Message;
                 return RedirectToAction("Index");
-            }
-
-        }
-        public IActionResult UserSave(UserModel userModel)
-        {
-            if (ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-
-            }
-            else
-            {
-                return View("Form", userModel);
-
             }
         }
     }
